2b831f96ff378b724d4c5f211936512cefbe012d
Mr. Devet <casey.devet@sccdsb.net>
2023-09-18 07:51:31 -0400

    Initial commit

---
+++ welcome.py
@@ -0,0 +1,12 @@
+# Programmer: 
+# Description: Ask the user about themselves and print a welcome message.
+
+# Print an initial greeting
+print "Hello!"
+
+# Ask the user some questions
+name - input(What is your name? )
+place = imput("Where are you from? ")
+
+# Print this information back
+Print("Welcome {name} from {place}!")
